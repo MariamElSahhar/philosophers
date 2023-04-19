2f67a6d37ba8cf6f4597225bdb89b94f37e56e87
MariamElSahhar <me1693@nyu.edu>
2023-04-19 16:33:54 +0400

    removed global variables

--- philo/main.c
+++ philo/main.c
@@ -6,61 +6,72 @@
 /*   By: melsahha <melsahha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/14 11:58:17 by melsahha          #+#    #+#             */
-/*   Updated: 2023/04/17 15:28:29 by melsahha         ###   ########.fr       */
+/*   Updated: 2023/04/19 15:55:02 by melsahha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
-pthread_mutex_t	forks[5];
-pthread_mutex_t	eat_mutex;
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 
-int	next_fork(int index)
+void *eat(void *args)
 {
-	index ++;
-	if (index == 5)
-		return (0);
-	return (index);
+	t_args *eat_args = (t_args *)args;
+	pthread_mutex_lock(eat_args->eat_mutex);
+	int philo_id = eat_args->philo_id;
+
+	pthread_mutex_lock(eat_args->fork1);
+	pthread_mutex_lock(eat_args->fork2);
+
+	printf("philo %i is eating using forks %i and %i\n", philo_id, philo_id, (philo_id + 1) % 5);
+
+	pthread_mutex_unlock(eat_args->fork2);
+	pthread_mutex_unlock(eat_args->fork1);
+	pthread_mutex_unlock(eat_args->eat_mutex);
+
+	return NULL;
 }
 
-
-void	*eat(void *i)
+int main(void)
 {
-	int	num;
-	int	fork1;
-	int	fork2;
+	pthread_t philos[5];
+	pthread_mutex_t *forks = malloc(sizeof(pthread_mutex_t) * 5);
+	pthread_mutex_t eat_mutex;
 
-	pthread_mutex_lock(&eat_mutex);
-	num = *((int*)i);
-	fork1 = num;
-	fork2 = next_fork(num);
-	pthread_mutex_lock(&forks[fork1]);
-	pthread_mutex_lock(&forks[fork2]);
-	printf("philo %i is eating using forks %i and %i\n", num, fork1, fork2);
-	pthread_mutex_unlock(&forks[fork1]);
-	pthread_mutex_unlock(&forks[fork2]);
-	pthread_mutex_unlock(&eat_mutex);
-	return (0);
-}
-
-int	main(void)
-{
-	pthread_t		philos[5];
-	int				*philo_id[5];
-
-	for (int i = 0; i < 5; i++)
-	{
-		philo_id[i] = (int *)ft_calloc(1, sizeof(int));
-		*philo_id[i] = i;
-		pthread_create(&philos[i], NULL, &eat, (void *) philo_id[i]);
-		pthread_mutex_init(&forks[i], NULL);
-	}
 	pthread_mutex_init(&eat_mutex, NULL);
 	for (int i = 0; i < 5; i++)
-	{
-		pthread_join(philos[i], NULL);
-		pthread_mutex_destroy(&forks[i]);
+		pthread_mutex_init(&forks[i], NULL);
+
+	for (int i = 0; i < 5; i++) {
+		t_args *eat_args = malloc(sizeof(t_args));
+		eat_args->philo_id = i;
+		eat_args->eat_mutex = &eat_mutex;
+		if (i == 4)
+		{
+			eat_args->fork1 = &forks[0];
+			eat_args->fork2 = &forks[i];
+		}
+		else
+		{
+			eat_args->fork1 = &forks[i];
+			eat_args->fork2 = &forks[i + 1];
+		}
+		pthread_create(&philos[i], NULL, &eat, (void *)eat_args);
+		free(eat_args);
 	}
-	pthread_mutex_destroy(&eat_mutex);
-	return (0);
+
+	for (int i = 0; i < 5; i++)
+		pthread_join(philos[i], NULL);
+
+	for (int i = 0; i < 5; i++)
+		pthread_mutex_destroy(&forks[i]);
+
+	free(forks);
+
+	return 0;
 }
+
+// valgrind --tool=helgrind ./philo
--- philo/philo.h
+++ philo/philo.h
@@ -6,7 +6,7 @@
 /*   By: melsahha <melsahha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/14 11:50:58 by melsahha          #+#    #+#             */
-/*   Updated: 2023/04/14 12:03:42 by melsahha         ###   ########.fr       */
+/*   Updated: 2023/04/19 15:51:10 by melsahha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,11 @@
 #include "../include/ft_printf.h"
 #include <pthread.h>
 
-
-
+typedef struct {
+	int philo_id;
+	pthread_mutex_t *eat_mutex;
+	pthread_mutex_t *fork1;
+	pthread_mutex_t *fork2;
+} t_args;
 
 #endif
